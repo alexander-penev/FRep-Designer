9b7baec5629a73fd770fb1d7115c1cdd2029ecb3
tomina.petrova <tomina.petrova@ef3f381e-c3b5-4d78-b0ca-7737329c0e22>
2012-09-12 04:29:56 +0000

    added library class, added basic class point, vector, ray

--- trunk/FRep-Designer/Model/Solid.cs
+++ trunk/FRep-Designer/Model/Solid.cs
@@ -4,9 +4,24 @@ namespace FRepDesigner
 {
     public abstract class Solid
     {
-        public Solid ()
+        public Solid()
         {
+           
         }
+
+        // prinadlejnist na tochka (sechenie s tochka)
+        public virtual bool Intersect(Point3D p)
+            {
+                return false;
+            }
+
+        //ray with silod
+        public virtual Point3D Intersect(Ray3D p)
+            {
+                return null;
+            }
+
+
     }
 }
 
