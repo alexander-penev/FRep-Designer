df58179cf8c808ed5507eaf040346d085ceddc44
tomina.petrova <tomina.petrova@ef3f381e-c3b5-4d78-b0ca-7737329c0e22>
2012-10-04 09:54:46 +0000

    just commit

---
+++ trunk/FRep-Designer/Test/FRep-Designer-Test/TestMath3D.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System;
+
+namespace FRepDesigner
+{
+    [TestFixture]
+    public class TestMath3D
+    {
+        [Test]
+        public void AddVectors()
+        {
+            //
+        }
+    }
+}
+
