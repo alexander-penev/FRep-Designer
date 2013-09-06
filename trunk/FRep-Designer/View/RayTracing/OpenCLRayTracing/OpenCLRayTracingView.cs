7b7f1a0c45f35e0381c5bad272c30ebd239646cc
rangel.ivanov <rangel.ivanov@ef3f381e-c3b5-4d78-b0ca-7737329c0e22>
2013-09-06 10:25:22 +0000

    New implementation of RayTracingView using opencl

--- trunk/FRep-Designer/View/RayTracing/OpenCLRayTracing/OpenCLRayTracingView.cs
+++ trunk/FRep-Designer/View/RayTracing/OpenCLRayTracing/OpenCLRayTracingView.cs
@@ -1,21 +1,46 @@
 using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 using Cairo;
 
 namespace FRepDesigner
 {
-    public class OpenCLRayTracingView: RayTracingView
-    {
-        public OpenCLRayTracingView(): base ()
-        {
-        }
+	public class OpenCLRayTracingView: RayTracingView
+	{
+		public OpenCLRayTracingView(): base ()
+		{
+			init();
+		}
 
-        public override void Render(Scene model, Gdk.Pixmap pixmap)
-        {
-            int width, height;
-            pixmap.GetSize(out width, out height);
-            // TODO: Render
-        }
-    }
+		~OpenCLRayTracingView()
+		{
+			destroy();
+		}
+
+		public override void Render(Scene model, Gdk.Pixmap pixmap)
+		{
+
+			int width, height;
+			pixmap.GetSize(out width, out height);
+			byte[] image = new byte[4 * width * height];
+			IntPtr p = Marshal.UnsafeAddrOfPinnedArrayElement (image, 0);
+			getImage (p, width, height);
+
+			pixmap.DrawRgb32Image (new Gdk.GC(pixmap), 0, 0, width, height, Gdk.RgbDither.None, image,  *4); 
+
+		}
+
+		[DllImport ("frep-cl-tracer")]
+		private static extern byte[] getImage(IntPtr p, int width, int height);
+
+		[DllImport ("frep-cl-tracer")]
+		private static extern void trace();
+
+		[DllImport ("frep-cl-tracer")]
+		private static extern void init();
+
+		[DllImport ("frep-cl-tracer")]
+		private static extern void destroy();
+	}
 }
